c2979043475ebae972602844ff8b0e8d36c5327e
Julián Gelvez <gelvez30@gmail.com>
2022-09-11 22:45:32 -0300

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const merried = false;
+  const persona = {
+    firstName: "David",
+    lastName: "Gallo",
+  };
 
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">
-      Hello world!
-    </h1>
-  )
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        {merried ? "Estoy casado" : "No estoy casado"}
+      </h1>
+      <h1 className="text-3xl font-bold underline text-yellow-500"> {persona.firstName} </h1>
+      <h2 className="text-3xl font-bold underline text-blue-500"> {persona.lastName} </h2>
+    </div>
+  );
 }
 
-export default App
+export default App;
